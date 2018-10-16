c7a00acaad0c1f75b772a25da46d83e167be373a
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2018-10-16 20:59:17 -0300

    Initial commit

---
+++ app/src/main/java/br/com/bicmsystems/minhabiblioteca/MainActivity.kt
@@ -0,0 +1,95 @@
+package br.com.bicmsystems.minhabiblioteca
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import br.com.bicmsystems.canivetesuico.demo.Calculadora
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSomar.setOnClickListener {
+
+            if(validarCampos()) {
+
+                val calc = Calculadora()
+                tvResultado.text = calc.somar(edtFirstNumber.text.toString().toInt(),
+                                                edtSecondNumber.text.toString().toInt()).toString()
+
+            } else {
+
+                Toast.makeText(this, "Informe o primeiro e segundo número", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+        btnSubtrair.setOnClickListener {
+
+            if(validarCampos()) {
+
+                val calc = Calculadora()
+                tvResultado.text = calc.subtrair(edtFirstNumber.text.toString().toInt(),
+                        edtSecondNumber.text.toString().toInt()).toString()
+
+            } else {
+
+                Toast.makeText(this, "Informe o primeiro e segundo número", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+
+        btnMultiplicar.setOnClickListener {
+
+            if(validarCampos()) {
+
+                val calc = Calculadora()
+                tvResultado.text = calc.multiplicar(edtFirstNumber.text.toString().toInt(),
+                        edtSecondNumber.text.toString().toInt()).toString()
+
+            } else {
+
+                Toast.makeText(this, "Informe o primeiro e segundo número", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+
+        btnDividir.setOnClickListener {
+
+            if(validarCampos(true)) {
+
+                val calc = Calculadora()
+                tvResultado.text = calc.dividir(edtFirstNumber.text.toString().toInt(),
+                        edtSecondNumber.text.toString().toInt()).toString()
+
+            } else {
+
+                Toast.makeText(this, "Informe o primeiro e segundo número (não pode ser igual a zero) ", Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+
+
+    }
+
+    private fun validarCampos(isDivisao: Boolean = false) : Boolean {
+
+        if(edtFirstNumber.text.isNullOrEmpty()) { return false }
+
+        if(edtSecondNumber.text.isNullOrEmpty()) { return false }
+
+        if(edtSecondNumber.text.toString().toInt() == 0) { return false }
+
+        return true
+
+    }
+
+
+
+}
