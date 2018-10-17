9acb203573ec6b7986c2516d4fa810a2363bfaef
Bruno Istvan Campos Monteiro <bruno.istvan@gmail.com>
2018-10-16 21:08:21 -0300

    Ajustes nas validações dos campos.

--- app/src/main/java/br/com/bicmsystems/minhabiblioteca/MainActivity.kt
+++ app/src/main/java/br/com/bicmsystems/minhabiblioteca/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -84,7 +85,7 @@ class MainActivity : AppCompatActivity() {
 
         if(edtSecondNumber.text.isNullOrEmpty()) { return false }
 
-        if(edtSecondNumber.text.toString().toInt() == 0) { return false }
+        if(isDivisao && edtSecondNumber.text.toString().toInt() == 0 ) { return false }
 
         return true
 
